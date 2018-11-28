0d9316a0dfae2062f7372da81d3c353996085050
BenardCharles <34279355+BenardCharles@users.noreply.github.com>
2018-11-28 14:52:01 +0300

    Add files via upload
    Upload the function

---
+++ benard/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='benard',
+      version='0.1',
+      description='The function prints hellow world once used',
+      url='https://github.com/BenardCharles/Hello-World-Package/benard',
+      author='Benard Charles',
+      author_email='benchars27@gmail.com',
+      license='MIT',
+      packages=['benard'],
+      zip_safe=False)
+
